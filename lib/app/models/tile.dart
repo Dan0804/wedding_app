826b015805472dc53a49b2074b04a9eb0959edf3
sojung.kim <thwjd2717@gmail.com>
2024-04-02 13:17:28 +0900

    chore: tile로 변수 동일하게 수정
    Github issue #3

--- lib/app/models/tile.dart
+++ lib/app/models/tile.dart
@@ -3,14 +3,14 @@ class Tile {
   final String tileTitle;
   final int budget;
   final DateTime deadline;
-  final String cardStatus;
+  final String tileStatus;
 
   Tile({
     required this.tileId,
     required this.tileTitle,
     required this.budget,
     required this.deadline,
-    required this.cardStatus,
+    required this.tileStatus,
   });
 
   factory Tile.fromJson(Map<String, dynamic> json) {
@@ -19,7 +19,7 @@ class Tile {
       tileTitle: json['cardTitle'],
       budget: json['budget'],
       deadline: DateTime.parse(json['deadline']),
-      cardStatus: json['cardStatus'],
+      tileStatus: json['cardStatus'],
     );
   }
 }
