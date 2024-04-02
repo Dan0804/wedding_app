2321a29f77eb802fe2ecc3fe691c7449ea0400b7
sojung.kim <thwjd2717@gmail.com>
2024-04-02 12:46:04 +0900

    feat: 카드 조회 리스트 UI
    Github issue #3

---
+++ lib/app/widgets/tile_list.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:wedding_app/app/models/tile.dart';
+
+import 'tile_widget.dart';
+
+
+class TileList extends StatelessWidget {
+  final String status;
+  final List<Tile> tiles;
+
+  TileList({required this.status, required this.tiles});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+            status,
+            style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: tiles.length,
+            itemBuilder: (context, index) {
+              return TileWidget(tile: tiles[index]);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
