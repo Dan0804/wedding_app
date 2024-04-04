cea5edb83064efa327019b4615b65e5f7510ff9a
sojung.kim <thwjd2717@gmail.com>
2024-04-05 00:46:51 +0900

    feat: 로그인 요청시 응답으로 온 토큰을 로컬 스토리지에 저장하는 로직 추가
    - 로그인이 되어 있지 않으면 로그인페이지 먼저 보여줌
    Github issue #14

--- lib/app/services/auth_service.dart
+++ lib/app/services/auth_service.dart
@@ -2,12 +2,27 @@ import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'package:shared_preferences/shared_preferences.dart';
+
 class AuthService extends ChangeNotifier {
+  bool _isLoggedIn = false;
+
+  bool get isLoggedIn => _isLoggedIn;
+
+  Future<bool> checkLoginStatus() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('token');
+
+    _isLoggedIn = token != null;
+    notifyListeners();
+
+    return _isLoggedIn;
+  }
 
   Future<void> login(String email, String password) async {
     String url = "localhost:8080";
     final response = await http.post(
-      Uri.http(url, '/api/v1/users/login'),
+      Uri.http(url, '/api/v1/auth/login'),
       body: json.encode({
         'email': email,
         'password': password,
@@ -20,6 +35,16 @@ class AuthService extends ChangeNotifier {
     );
 
     if (response.statusCode == 200) {
+      final responseData = json.decode(response.body);
+      final token = responseData['token'];
+
+      // 토큰을 로컬 스토리지에 저장
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('token', token);
+
+      _isLoggedIn = true;
+      notifyListeners();
+
       if (kDebugMode) {
         print('Login successful');
       }
