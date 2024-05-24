e9d80cfa5d071ffc92317a23f58e074cef54347b
Dan0804 <wheogus185@naver.com>
2024-05-25 00:38:32 +0900

    add localizations package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:wedding_app/app/services/auth_service.dart';
 import 'package:wedding_app/service_set/tile_service.dart';
@@ -29,6 +30,15 @@ class WeddingApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('en'),
+        Locale('ko'),
+      ],
       home: Consumer<AuthService>(
         builder: (context, authService, child) {
           authService.checkLoginStatus();
