d90e56e60586c9a731d3767ef9d87bdbd708bb6d
Dan0804 <wheogus185@naver.com>
2024-05-24 19:38:48 +0900

    linked remote server

--- lib/app/services/card/fetch_tile_api.dart
+++ lib/app/services/card/fetch_tile_api.dart
@@ -13,23 +13,15 @@ Future<List<Tile>> getTileApi(String cardStatus) async {
     print('fetchTileApi Token: $token');
   }
 
-  final response = await http.get(
-      Uri.parse('http://localhost:8080/api/v1/cardboard/$cardStatus'),
-      headers: {
-        'Authorization': 'Bearer $token',
-        "Content-Type": "application/json",
-        'Accept': '*/*'
-      }
-  );
+  final response =
+      await http.get(Uri.parse('http://49.50.163.210:8080/api/v1/cardboard/$cardStatus'), headers: {'Authorization': 'Bearer $token', "Content-Type": "application/json", 'Accept': '*/*'});
 
   if (response.statusCode == 200) {
     final List<dynamic> jsonList = json.decode(response.body);
     return jsonList.map((json) => Tile.fromJson(json)).toList();
   } else {
-    throw Exception(
-        'Failed to load tiles from the API. '
+    throw Exception('Failed to load tiles from the API. '
         'Status code: ${response.statusCode} '
-        'Response body: ${json.decode(response.body)}'
-    );
+        'Response body: ${json.decode(response.body)}');
   }
 }
