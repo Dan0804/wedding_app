3136260565d77af47e438a4182b7bcb02222773b
Dan0804 <wheogus185@naver.com>
2024-05-24 20:58:33 +0900

    removed spacer

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:wedding_app/app/services/auth_service.dart';
 import 'package:wedding_app/service_set/tile_service.dart';
-
 import 'app/screens/login_screen.dart';
 import 'app/screens/main_screen.dart';
 
