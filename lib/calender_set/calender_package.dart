274ec84fdad1b88ffd035896030e96648ed1101f
Dan0804 <wheogus185@naver.com>
2024-05-29 05:15:04 +0900

    removed flexible above Card

--- lib/calender_set/calender_package.dart
+++ lib/calender_set/calender_package.dart
@@ -13,71 +13,68 @@ class _CalenderPackageState extends State<CalenderPackage> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Flexible(
-        fit: FlexFit.tight,
-        child: SizedBox(
-          width: double.infinity,
-          height: double.infinity,
-          child: Card(
-            elevation: 10,
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      "Calender",
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
+      child: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Card(
+          elevation: 10,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Calender",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Center(
+                  child: Stack(
+                    children: [
+                      Calender(
+                        textTile: false,
+                        setDate: null,
                       ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  Center(
-                    child: Stack(
-                      children: [
-                        Calender(
-                          textTile: false,
-                          setDate: null,
+                      Positioned(
+                        top: 12,
+                        right: 64,
+                        child: IconButton(
+                          onPressed: () {
+                            setState(() {});
+                          },
+                          icon: Icon(Icons.refresh),
                         ),
-                        Positioned(
-                          top: 12,
-                          right: 64,
-                          child: IconButton(
-                            onPressed: () {
-                              setState(() {});
-                            },
-                            icon: Icon(Icons.refresh),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 24,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black45),
                       ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black45),
                     ),
                   ),
-                  SizedBox(
-                    height: 24,
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: CalenderDetail(
+                    textTile: false,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: CalenderDetail(
-                      textTile: false,
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
