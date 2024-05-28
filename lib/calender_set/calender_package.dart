7fc2f41f179a1babbf49a4a75eb65ff0c7b68d94
Dan0804 <wheogus185@naver.com>
2024-05-28 23:47:18 +0900

    modifying calender title layout

--- lib/calender_set/calender_package.dart
+++ lib/calender_set/calender_package.dart
@@ -13,80 +13,74 @@ class _CalenderPackageState extends State<CalenderPackage> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          SizedBox(
-            width: double.infinity,
-            child: Row(
-              children: [
-                SizedBox(
-                  width: 12,
-                ),
-                Text(
-                  "Calender",
-                  style: TextStyle(
-                    fontSize: 24,
+      child: Flexible(
+        fit: FlexFit.tight,
+        child: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Card(
+            elevation: 10,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Calender",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ),
-          Flexible(
-            fit: FlexFit.tight,
-            child: SizedBox(
-              width: double.infinity,
-              child: Card(
-                elevation: 10,
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      Center(
-                        child: Stack(
-                          children: [
-                            Calender(
-                              textTile: false,
-                              setDate: null,
-                            ),
-                            Positioned(
-                              top: 12,
-                              right: 64,
-                              child: IconButton(
-                                onPressed: () {
-                                  setState(() {});
-                                },
-                                icon: Icon(Icons.refresh),
-                              ),
-                            ),
-                          ],
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Center(
+                    child: Stack(
+                      children: [
+                        Calender(
+                          textTile: false,
+                          setDate: null,
                         ),
-                      ),
-                      SizedBox(
-                        height: 24,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black45),
+                        Positioned(
+                          top: 12,
+                          right: 64,
+                          child: IconButton(
+                            onPressed: () {
+                              setState(() {});
+                            },
+                            icon: Icon(Icons.refresh),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 24,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                        child: CalenderDetail(
-                          textTile: false,
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black45),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: CalenderDetail(
+                      textTile: false,
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
-        ],
+        ),
       ),
     );
   }
