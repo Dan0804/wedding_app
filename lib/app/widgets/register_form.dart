6266ea1f1a00b6d145d88d538d3d22d3b9f7e189
Dan0804 <wheogus185@naver.com>
2024-06-05 01:18:03 +0900

    seperated partner email certification form from register form

--- lib/app/widgets/register_form.dart
+++ lib/app/widgets/register_form.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:wedding_app/app/services/auth_service.dart';
+import 'package:wedding_app/app/widgets/partner_enter_pop_up.dart';
 
 class RegisterForm extends StatefulWidget {
   const RegisterForm({super.key});
@@ -16,7 +15,6 @@ class _RegisterFormState extends State<RegisterForm> {
   final _passwordController = TextEditingController();
   final _passwordCheckController = TextEditingController();
   final _nameController = TextEditingController();
-  final _partnerEmailController = TextEditingController();
 
   var showEmailCerti = false;
   var showRegisterForm = false;
@@ -77,42 +75,25 @@ class _RegisterFormState extends State<RegisterForm> {
         SizedBox(
           height: 20,
         ),
-        SizedBox(
-          width: 300,
-          child: TextFormField(
-            controller: _partnerEmailController,
-            decoration: InputDecoration(labelText: 'Partner Email (나중에 입력해도 되요!)'),
-            validator: (value) {
-              if (value == '') {
-                return null;
-              } else if (!RegExp(r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?").hasMatch(value!)) {
-                return '이메일 형식이 틀립니다.';
-              }
-              return null;
-            },
-          ),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              child: Text('가입할게요!'),
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  Provider.of<AuthService>(context, listen: false).register(
-                    _emailController.text,
-                    _passwordController.text,
-                    _nameController.text,
-                    _partnerEmailController.text,
-                    context,
-                  );
-                }
-              },
-            ),
-          ],
+        ElevatedButton(
+          child: Text('가입할게요!'),
+          onPressed: () {
+            if (_formKey.currentState!.validate()) {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return PartnerEnterPopUp();
+                  });
+
+              // Provider.of<AuthService>(context, listen: false).register(
+              //   _emailController.text,
+              //   _passwordController.text,
+              //   _nameController.text,
+              //   _partnerEmailController.text,
+              //   context,
+              // );
+            }
+          },
         ),
       ],
     );
@@ -146,17 +127,20 @@ class _RegisterFormState extends State<RegisterForm> {
                 ),
                 showRegisterForm
                     ? Container()
-                    : ElevatedButton(
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            showEmailCerti = true;
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(content: Text('인증 코드를 보냈어요!')),
-                            );
-                            setState(() {});
-                          }
-                        },
-                        child: Text('인증 요청'),
+                    : SizedBox(
+                        width: 120,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              showEmailCerti = true;
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text('인증 코드를 보냈어요!')),
+                              );
+                              setState(() {});
+                            }
+                          },
+                          child: Text('인증 요청'),
+                        ),
                       ),
               ],
             ),
@@ -180,18 +164,21 @@ class _RegisterFormState extends State<RegisterForm> {
                           },
                         ),
                       ),
-                      ElevatedButton(
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            showRegisterForm = true;
-                            showEmailCerti = false;
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(content: Text('이메일 인증에 성공했어요. 나머지 정보를 입력해주세요!')),
-                            );
-                            setState(() {});
-                          }
-                        },
-                        child: Text('인증 확인'),
+                      SizedBox(
+                        width: 120,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              showRegisterForm = true;
+                              showEmailCerti = false;
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text('이메일 인증에 성공했어요. 나머지 정보를 입력해주세요!')),
+                              );
+                              setState(() {});
+                            }
+                          },
+                          child: Text('인증 확인'),
+                        ),
                       ),
                     ],
                   )
