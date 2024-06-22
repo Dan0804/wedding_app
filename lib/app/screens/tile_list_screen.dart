6f9b6453648614e13d3781b3997f662a47fccb43
Dan0804 <wheogus185@naver.com>
2024-06-23 01:03:54 +0900

    added create card button on List widget

--- lib/app/screens/tile_list_screen.dart
+++ lib/app/screens/tile_list_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:wedding_app/app/screens/create_card_screen.dart';
 import 'package:wedding_app/app/services/card/fetch_tile_api.dart';
 import '../widgets/tile_list.dart';
 
@@ -20,59 +21,96 @@ class _TileListScreenState extends State<TileListScreen> {
             var datas = snapshot.data ?? [];
             return Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Card(
-                elevation: 10,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        'Wedding To Do List',
-                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              child: Stack(
+                children: [
+                  Card(
+                    elevation: 10,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            'Wedding To Do List',
+                            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 16,
+                          ),
+                          datas.isEmpty
+                              ? Center(
+                                  child: CircularProgressIndicator(),
+                                )
+                              : Flexible(
+                                  fit: FlexFit.tight,
+                                  child: Row(
+                                    children: [
+                                      Expanded(
+                                        child: TileList(
+                                          status: 'BACKLOG',
+                                          tiles: datas[0],
+                                        ),
+                                      ),
+                                      VerticalDivider(
+                                        width: 1,
+                                        color: Colors.grey,
+                                      ),
+                                      Expanded(
+                                        child: TileList(
+                                          status: 'PROGRESS',
+                                          tiles: datas[1],
+                                        ),
+                                      ),
+                                      VerticalDivider(
+                                        width: 1,
+                                        color: Colors.grey,
+                                      ),
+                                      Expanded(
+                                        child: TileList(
+                                          status: 'DONE',
+                                          tiles: datas[2],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                        ],
                       ),
-                      SizedBox(
-                        height: 16,
-                      ),
-                      datas.isEmpty
-                          ? Center(
-                              child: CircularProgressIndicator(),
-                            )
-                          : Flexible(
-                              fit: FlexFit.tight,
-                              child: Row(
-                                children: [
-                                  Expanded(
-                                    child: TileList(
-                                      status: 'BACKLOG',
-                                      tiles: datas[0],
-                                    ),
-                                  ),
-                                  VerticalDivider(
-                                    width: 1,
-                                    color: Colors.grey,
-                                  ),
-                                  Expanded(
-                                    child: TileList(
-                                      status: 'PROGRESS',
-                                      tiles: datas[1],
-                                    ),
-                                  ),
-                                  VerticalDivider(
-                                    width: 1,
-                                    color: Colors.grey,
-                                  ),
-                                  Expanded(
-                                    child: TileList(
-                                      status: 'DONE',
-                                      tiles: datas[2],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                    ],
+                    ),
                   ),
-                ),
+                  Positioned(
+                    top: 16,
+                    left: 20,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        elevation: 5,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => CreateCardScreen()),
+                        );
+                      },
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.add,
+                          ),
+                          Text(
+                            'New Card 생성',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             );
           });
