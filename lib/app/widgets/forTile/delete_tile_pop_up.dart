95518f2f71789f5238e3ac145242839a2c232f9d
Dan0804 <wheogus185@naver.com>
2024-06-26 20:08:57 +0900

    edit button layout

--- lib/app/widgets/forTile/delete_tile_pop_up.dart
+++ lib/app/widgets/forTile/delete_tile_pop_up.dart
@@ -24,61 +24,97 @@ class _DeleteTilePopUpState extends State<DeleteTilePopUp> {
         child: Text('Card 삭제 Page'),
       ),
       content: SizedBox(
-          height: 300,
-          width: 400,
-          child: Column(
-            children: [
-              Text('아래 Card를 정말 삭제하시겠습니까?'),
-              Card(
-                elevation: 10,
-                child: Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            widget.tile.tileTitle,
-                            style: TextStyle(
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.bold,
-                            ),
+        height: 300,
+        width: 400,
+        child: Column(
+          children: [
+            Text(
+              '아래 Card를 정말 삭제하시겠습니까?',
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 16,
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Card(
+              elevation: 6,
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.tile.tileTitle,
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
                           ),
-                          SizedBox(height: 8.0),
-                          Text('Budget: ${NumberFormat('₩###,###,###,###').format(widget.tile.budget)}'),
-                          widget.tile.deadline != DateTime(1994, 8, 4) ? Text('Deadline: ${DateFormat('yyyy-MM-dd').format(widget.tile.deadline)}') : Text('Deadline: 미정'),
-                        ],
-                      ),
-                    ],
-                  ),
+                        ),
+                        SizedBox(height: 8.0),
+                        Text('Budget: ${NumberFormat('₩###,###,###,###').format(widget.tile.budget)}'),
+                        widget.tile.deadline != DateTime(1994, 8, 4) ? Text('Deadline: ${DateFormat('yyyy-MM-dd').format(widget.tile.deadline)}') : Text('Deadline: 미정'),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-            ],
-          )),
-      actions: <Widget>[
-        Consumer<TileService>(builder: (context, tileService, child) {
-          return TextButton(
-            child: const Text('삭제하기!'),
-            onPressed: () async {
-              bool isSuccess = await tileService.deleteTile(
-                widget.tile.tileId,
-              );
-              if (isSuccess) {
-                Navigator.pop(context);
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text('Delete Created Successfully!')),
-                );
-              } else {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text('Failed to Delete Card')),
-                );
-              }
-            },
-          );
-        }),
-      ],
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Consumer<TileService>(
+                  builder: (context, tileService, child) {
+                    return SizedBox(
+                      width: 140,
+                      child: ElevatedButton(
+                        child: const Text('삭제하기!'),
+                        onPressed: () async {
+                          bool isSuccess = await tileService.deleteTile(
+                            widget.tile.tileId,
+                          );
+                          if (isSuccess) {
+                            Navigator.pop(context);
+                            Navigator.pop(context);
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text('Delete Created Successfully!')),
+                            );
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text('Failed to Delete Card')),
+                            );
+                          }
+                        },
+                      ),
+                    );
+                  },
+                ),
+                SizedBox(
+                  width: 40,
+                ),
+                SizedBox(
+                  width: 140,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      '아니요!',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
