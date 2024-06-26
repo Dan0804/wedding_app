d699d6a66d9868d7f3e31b4850c1ef87b7f36aff
Dan0804 <wheogus185@naver.com>
2024-06-26 19:45:27 +0900

    remove unused Stack widget

--- lib/app/widgets/forCalender/calender.dart
+++ lib/app/widgets/forCalender/calender.dart
@@ -61,137 +61,133 @@ class _CalenderState extends State<Calender> {
     return Consumer<TileService>(
       builder: (context, tileService, child) {
         tileService.firstGetTilesForDay(_today);
-        return Stack(
-          children: [
-            TableCalendar(
-              calendarBuilders: CalendarBuilders(
-                todayBuilder: (context, day, focusedDay) {
-                  final text = day.day.toString();
-                  return Padding(
-                    padding: const EdgeInsets.only(top: 6, left: 6, right: 6, bottom: 4),
-                    child: AnimatedContainer(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.rectangle,
-                        color: Colors.blue[500],
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                      duration: Duration(microseconds: 2000),
-                      child: Align(
-                        alignment: Alignment(-0.25, -0.5),
-                        child: Text(
-                          text,
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                  );
-                },
-                defaultBuilder: (context, day, focusedDay) {
-                  final text = day.day.toString();
-
-                  return Align(
-                    alignment: Alignment(-0.25, -0.25),
+        return TableCalendar(
+          calendarBuilders: CalendarBuilders(
+            todayBuilder: (context, day, focusedDay) {
+              final text = day.day.toString();
+              return Padding(
+                padding: const EdgeInsets.only(top: 6, left: 6, right: 6, bottom: 4),
+                child: AnimatedContainer(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    color: Colors.blue[500],
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  duration: Duration(microseconds: 2000),
+                  child: Align(
+                    alignment: Alignment(-0.25, -0.5),
                     child: Text(
                       text,
                       style: TextStyle(
-                          color: day.weekday == DateTime.saturday
-                              ? Colors.blue
-                              : day.weekday == DateTime.sunday
-                                  ? Colors.red
-                                  : null),
+                        color: Colors.white,
+                      ),
                     ),
-                  );
-                },
-                outsideBuilder: (context, day, focusedDay) {
-                  final text = day.day.toString();
+                  ),
+                ),
+              );
+            },
+            defaultBuilder: (context, day, focusedDay) {
+              final text = day.day.toString();
 
-                  return Align(
-                    alignment: Alignment(-0.25, -0.25),
-                    child: Text(
-                      text,
-                      style: TextStyle(color: Colors.black45),
+              return Align(
+                alignment: Alignment(-0.25, -0.25),
+                child: Text(
+                  text,
+                  style: TextStyle(
+                      color: day.weekday == DateTime.saturday
+                          ? Colors.blue
+                          : day.weekday == DateTime.sunday
+                              ? Colors.red
+                              : null),
+                ),
+              );
+            },
+            outsideBuilder: (context, day, focusedDay) {
+              final text = day.day.toString();
+
+              return Align(
+                alignment: Alignment(-0.25, -0.25),
+                child: Text(
+                  text,
+                  style: TextStyle(color: Colors.black45),
+                ),
+              );
+            },
+            dowBuilder: (context, day) {
+              final text = DateFormat.E().format(day);
+
+              return Center(
+                child: Text(
+                  text,
+                  style: TextStyle(
+                      color: day.weekday == DateTime.saturday
+                          ? Colors.blue
+                          : day.weekday == DateTime.sunday
+                              ? Colors.red
+                              : null),
+                ),
+              );
+            },
+            selectedBuilder: (context, day, focusedDay) {
+              final text = day.day.toString();
+
+              return Padding(
+                padding: const EdgeInsets.only(top: 2, left: 2, right: 2),
+                child: GestureDetector(
+                  onDoubleTap: () {
+                    widget.setDate!(_selectedDay);
+                    Navigator.pop(context);
+                  },
+                  child: AnimatedContainer(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Colors.purple[200],
+                      borderRadius: BorderRadius.circular(8.0),
                     ),
-                  );
-                },
-                dowBuilder: (context, day) {
-                  final text = DateFormat.E().format(day);
-
-                  return Center(
-                    child: Text(
-                      text,
-                      style: TextStyle(
-                          color: day.weekday == DateTime.saturday
-                              ? Colors.blue
-                              : day.weekday == DateTime.sunday
-                                  ? Colors.red
-                                  : null),
-                    ),
-                  );
-                },
-                selectedBuilder: (context, day, focusedDay) {
-                  final text = day.day.toString();
-
-                  return Padding(
-                    padding: const EdgeInsets.only(top: 2, left: 2, right: 2),
-                    child: GestureDetector(
-                      onDoubleTap: () {
-                        widget.setDate!(_selectedDay);
-                        Navigator.pop(context);
-                      },
-                      child: AnimatedContainer(
-                        decoration: BoxDecoration(
-                          shape: BoxShape.rectangle,
-                          color: Colors.purple[200],
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                        duration: Duration(microseconds: 2000),
-                        child: Align(
-                          alignment: Alignment(-0.6, -0.75),
-                          child: Text(
-                            text,
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.white,
-                            ),
-                          ),
+                    duration: Duration(microseconds: 2000),
+                    child: Align(
+                      alignment: Alignment(-0.6, -0.75),
+                      child: Text(
+                        text,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
                         ),
                       ),
                     ),
-                  );
-                },
-                markerBuilder: (context, day, events) {
-                  if (events.isNotEmpty) {
-                    return _eventsMarker(events, day, _selectedDay);
-                  } else {
-                    return null;
-                  }
-                },
-              ),
-              focusedDay: _today,
-              firstDay: DateTime.utc(_year - 2),
-              lastDay: DateTime.utc(_year + 3),
-              onDaySelected: (day, focusedDay) {
-                setState(() {
-                  _selectedDay = day;
-                  _today = focusedDay;
-                  tileService.getTilesForDay(_selectedDay);
-                });
-              },
-              selectedDayPredicate: (day) {
-                return isSameDay(_selectedDay, day);
-              },
-              availableGestures: AvailableGestures.all,
-              headerStyle: HeaderStyle(
-                formatButtonVisible: false,
-                titleCentered: true,
-              ),
-              eventLoader: (day) {
-                return getEventsForDay(tileService.calendarTiles, day);
-              },
-            ),
-          ],
+                  ),
+                ),
+              );
+            },
+            markerBuilder: (context, day, events) {
+              if (events.isNotEmpty) {
+                return _eventsMarker(events, day, _selectedDay);
+              } else {
+                return null;
+              }
+            },
+          ),
+          focusedDay: _today,
+          firstDay: DateTime.utc(_year - 2),
+          lastDay: DateTime.utc(_year + 3),
+          onDaySelected: (day, focusedDay) {
+            setState(() {
+              _selectedDay = day;
+              _today = focusedDay;
+              tileService.getTilesForDay(_selectedDay);
+            });
+          },
+          selectedDayPredicate: (day) {
+            return isSameDay(_selectedDay, day);
+          },
+          availableGestures: AvailableGestures.all,
+          headerStyle: HeaderStyle(
+            formatButtonVisible: false,
+            titleCentered: true,
+          ),
+          eventLoader: (day) {
+            return getEventsForDay(tileService.calendarTiles, day);
+          },
         );
       },
     );
